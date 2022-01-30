cc9ce6deb46dafeb696d0829f4c1f05091f5a460
Alex <china19881@rambler.ru>
2022-01-30 11:12:34 +0800

    finished Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+string [] array = {"Hello", "2", "World", ":-)","123","-2"};
+
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+    {
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+    }
+    
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
